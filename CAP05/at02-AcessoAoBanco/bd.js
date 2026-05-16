aeda6b80623f73d2894b64112ed5e6ab9066e4b0
ALVARO LUIS DE TOLEDO BAPTISTELLA <alvaro.ltbaptistella@senacsp.edu.br>
2026-05-16 12:05:54 -0300

    cadastro BD

---
+++ CAP05/at02-AcessoAoBanco/bd.js
@@ -0,0 +1,13 @@
+import mysql from 'mysql2/promise'
+
+const dbConnection = async () => {
+  const connection = await mysql.createConnection({
+    host: process.env.DB_HOST, // "localhost"
+    user: process.env.DB_USER, // "root"
+    password: process.env.DB_PASSWORD, //""
+    database: process.env.DB_NAME //"amigo_do_pet"
+  })
+  return connection
+}
+
+export default dbConnection
